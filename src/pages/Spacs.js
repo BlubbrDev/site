a819577dc23f8568f575440b69160a6af7607d08
Justin Shaw <thoushawnotpass@gmail.com>
2021-03-03 20:39:56 -0800

    updated linter and spacs page

--- src/pages/Spacs.js
+++ src/pages/Spacs.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 // This page contains an embedded dashboard of spac information, powered by Tableau.
 // If you have any questions, you should contact @StevenK
 export default function Spacs() {
@@ -16,13 +18,13 @@ export default function Spacs() {
     vizElement.parentNode.insertBefore(scriptElement, vizElement);
 
     return (
-        <div class='tableauPlaceholder' id='viz1614816342907' style={{ 'position': 'relative' }}>
+        <div className='tableauPlaceholder' id='viz1614816342907' style={{ 'position': 'relative' }}>
             <noscript>
                 <a href='/'>
                     <img alt=' ' src='https://public.tableau.com/static/images/Bl/BlubbrVisualizations/NotiFiDataDashboard_1/1_rss.png' style={{ 'border': 'none' }} />
                 </a>
             </noscript>
-            <object class='tableauViz' style={{ 'display': 'none' }}>
+            <object className='tableauViz' style={{ 'display': 'none' }}>
                 <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
                 <param name='embed_code_version' value='3' />
                 <param name='site_root' value='' />
