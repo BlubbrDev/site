1eff1a74e0722d64c849807dcecfd3ceed1ca30b
Justin Shaw <thoushawnotpass@gmail.com>
2021-03-03 20:24:29 -0800

    added seperate spacs js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Spacs from './pages/Spacs.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to <code>blubbr.io</code>, Soren Rood!
-        </p>
+        <Spacs />
       </header>
     </div>
   );
