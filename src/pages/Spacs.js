cfb85f63a47d33687f3f2f87aaaf11e53809445c
Justin Shaw <thoushawnotpass@gmail.com>
2021-03-04 17:12:04 -0800

    compiles with no errors but doesn't work

--- src/pages/Spacs.js
+++ src/pages/Spacs.js
@@ -1,40 +1,44 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 // This page contains an embedded dashboard of spac information, powered by Tableau.
 // If you have any questions, you should contact @StevenK
 function Spacs() {
-  let divElement = document.getElementById("tableau-container");
-  let vizElement = document.getElementById("tableau-visualization");
-  let width = divElement.offsetWidth;
-  vizElement.style.width = "100%";
-  if (width > 800 || width > 500) {
-    vizElement.style.height = divElement.offsetWidth * 0.75 + "px";
-    vizElement.style.height = divElement.offsetWidth * 0.75 + "px";
-  } else {
-    vizElement.style.height = "1127px";
-  }
-  let scriptElement = document.createElement("script");
-  scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
-  vizElement.parentNode.insertBefore(scriptElement, vizElement);
+  const tableauContainer = useRef(null);
+  const tableauViz = useRef(null);
+
+  useEffect(() => {
+    tableauViz.current.style.width = "100%";
+    if (tableauContainer.current.offsetWidth > 800) {
+      tableauViz.current.style.height =
+        tableauContainer.current.offsetWidth * 0.75 + "px";
+    } else if (tableauContainer.current.offsetWidth > 500) {
+      tableauViz.current.style.height =
+        tableauContainer.current.offsetWidth * 0.75 + "px";
+    } else {
+      tableauViz.current.style.height = "1127px";
+    }
+    var scriptElement = document.createElement("script");
+    scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
+    tableauViz.current.parentNode.insertBefore(
+      scriptElement,
+      tableauViz.current
+    );
+  });
 
   return (
-    <div
-      className="tableauPlaceholder"
-      id="tableau-container"
-      style={{ position: "relative" }}
-    >
+    <div ref={tableauContainer} style={{ position: "relative" }}>
       <noscript>
         <a href="/">
           <img
-            alt="Blubbr SPAC Visualizations"
+            alt="Blubbr SPAC Visualization"
             src="https://public.tableau.com/static/images/Bl/BlubbrVisualizations/NotiFiDataDashboard_1/1_rss.png"
             style={{ border: "none" }}
           />
         </a>
       </noscript>
-      <object id="tableau-visualization" style={{ display: "none" }}>
-        <param name="host_url" value="https://public.tableau.com/" />
-        <param name="embed_code_version" value="3" />
+      <object ref={tableauViz} style={{ display: "none" }}>
+        <param name="host_url" value="https://public.tableau.com/" />{" "}
+        <param name="embed_code_version" value="3" />{" "}
         <param name="site_root" value="" />
         <param name="name" value="BlubbrVisualizations/NotiFiDataDashboard_1" />
         <param name="tabs" value="no" />
@@ -42,7 +46,7 @@ function Spacs() {
         <param
           name="static_image"
           value="https://public.tableau.com/static/images/Bl/BlubbrVisualizations/NotiFiDataDashboard_1/1.png"
-        />
+        />{" "}
         <param name="animate_transition" value="yes" />
         <param name="display_static_image" value="yes" />
         <param name="display_spinner" value="yes" />
