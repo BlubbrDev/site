a2e8ad5e7b7a688824b28e112218ec33e5d98dbd
Justin Shaw <thoushawnotpass@gmail.com>
2021-03-08 19:23:05 -0800

    ugh this is gonna take a while

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from "react";
+
 // import React, { useEffect, useRef } from "react";
 // import { useLocation } from "react-router-dom";
 
-import PageRouter from "utils/PageRouter";
-// import LayoutDefault from "layouts/LayoutDefault";
-
 // Initialize Google Analytics
 // ReactGA.initialize(process.env.REACT_APP_GA_CODE);
+
 // const trackPage = (page) => {
 //   ReactGA.set({ page });
 //   ReactGA.pageview(page);
 // };
+
+import PageRouter from "utils/PageRouter";
+// import LayoutDefault from "layouts/LayoutDefault";
 
 export default function App() {
   // let location = useLocation();
@@ -20,12 +22,5 @@ export default function App() {
   //   document.body.classList.add("is-loaded");
   //   // trackPage(page);
   // }, [location]);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <PageRouter />
-      </header>
-    </div>
-  );
+  return <PageRouter />;
 }
---
+++ src/layouts/LayoutDefault.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from 'components/layout/Header';
+import Footer from 'components/layout/Footer';
+
+interface LayoutDefaultProps {
+  children: JSX.Element[] | JSX.Element;
+}
+
+export default function LayoutDefault(props: LayoutDefaultProps) {
+  return (
+    <>
+      <Header navPosition="right" className="reveal-from-bottom" />
+      <main className="site-content">{props.children}</main>
+      <Footer />
+    </>
+  );
+}
