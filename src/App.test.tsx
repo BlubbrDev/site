1e8f3b11caf7c5cc5674fb2ea229e9ccac2be616
Justin Shaw <thoushawnotpass@gmail.com>
2021-03-08 17:16:04 -0800

    updated tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import ReactDOM from "react-dom";
+
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
 });
