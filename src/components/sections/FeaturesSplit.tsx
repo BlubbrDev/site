0dbdb88f9e1e16c2d6ee1df6ed651fb1a6464bb3
Justin Shaw <thoushawnotpass@gmail.com>
2021-03-09 22:46:07 -0800

    features split component added

---
+++ src/components/sections/FeaturesSplit.tsx
@@ -0,0 +1,147 @@
+import React from "react";
+import classNames from "classnames";
+import {
+  SectionSplitProps,
+  defaultSectionSplitProps,
+} from "utils/SectionProps";
+import SectionHeader from "./partials/SectionHeader";
+import Image from "components/elements/Image";
+
+export default function FeaturesSplit(
+  props: SectionSplitProps = defaultSectionSplitProps
+) {
+  const outerClasses = classNames(
+    "features-split section",
+    props.topOuterDivider && "has-top-divider",
+    props.bottomOuterDivider && "has-bottom-divider",
+    props.hasBgColor && "has-bg-color",
+    props.invertColor && "invert-color",
+    props.className
+  );
+
+  const innerClasses = classNames(
+    "features-split-inner section-inner",
+    props.topDivider && "has-top-divider",
+    props.bottomDivider && "has-bottom-divider"
+  );
+
+  const splitClasses = classNames(
+    "split-wrap",
+    props.invertMobile && "invert-mobile",
+    props.invertDesktop && "invert-desktop",
+    props.alignTop && "align-top"
+  );
+
+  const sectionHeader = {
+    title: "Workflow that just works",
+    paragraph:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.",
+  };
+
+  return (
+    <section {...props} className={outerClasses}>
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={splitClasses}>
+            <div className="split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-left"
+                data-reveal-container=".split-item"
+              >
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Lightning fast workflow
+                </div>
+                <h3 className="mt-0 mb-12">Data-driven insights</h3>
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                  — Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </div>
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  props.imageFill && "split-item-image-fill"
+                )}
+                data-reveal-container=".split-item"
+              >
+                <Image
+                  src={require("./../../assets/images/features-split-image-01.png")}
+                  alt="Features split 01"
+                  width={528}
+                  height={396}
+                />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-right"
+                data-reveal-container=".split-item"
+              >
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Lightning fast workflow
+                </div>
+                <h3 className="mt-0 mb-12">Data-driven insights</h3>
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                  — Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </div>
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  props.imageFill && "split-item-image-fill"
+                )}
+                data-reveal-container=".split-item"
+              >
+                <Image
+                  src={require("./../../assets/images/features-split-image-02.png")}
+                  alt="Features split 02"
+                  width={528}
+                  height={396}
+                />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-left"
+                data-reveal-container=".split-item"
+              >
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Lightning fast workflow
+                </div>
+                <h3 className="mt-0 mb-12">Data-driven insights</h3>
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                  — Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </div>
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  props.imageFill && "split-item-image-fill"
+                )}
+                data-reveal-container=".split-item"
+              >
+                <Image
+                  src={require("./../../assets/images/features-split-image-03.png")}
+                  alt="Features split 03"
+                  width={528}
+                  height={396}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import Hero from "components/sections/Hero";
 import LayoutDefault from "layouts/LayoutDefault";
 import FeaturesTiles from "components/sections/FeaturesTiles";
+import FeaturesSplit from "components/sections/FeaturesSplit";
 
 export default function Home() {
   return (
     <LayoutDefault>
       <Hero className="illustration-section-01" />
       <FeaturesTiles />
+      <FeaturesSplit
+        invertMobile
+        topDivider
+        imageFill
+        className="illustration-section-02"
+      />
     </LayoutDefault>
   );
 }
