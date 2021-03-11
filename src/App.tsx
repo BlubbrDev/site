4d767519526bf2ca988e375c086ba0e9649ca163
Justin Shaw <thoushawnotpass@gmail.com>
2021-03-10 20:05:14 -0800

    added google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,9 @@ import React, { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import PageRouter from "utils/PageRouter";
 import ScrollReveal from "utils/ScrollReveal";
-import ReactGA from "react-ga";
+import { initializeGoogleAnalytics } from "utils/GoogleAnalytics";
 
-// Initialize Google Analytics
-ReactGA.initialize(process.env.REACT_APP_GA_CODE as string);
-
-const trackPage = (page: string) => {
-  ReactGA.set({ page });
-  ReactGA.pageview(page);
-};
+initializeGoogleAnalytics();
 
 export default function App() {
   const childRef = useRef<any>();
@@ -19,8 +13,8 @@ export default function App() {
   useEffect(() => {
     document.body.classList.add("is-loaded");
     childRef.current.init();
-    trackPage(location.pathname);
   }, [location]);
+
   return (
     <ScrollReveal ref={childRef}>
       <PageRouter />
--- src/utils/PageRouter.tsx
+++ src/utils/PageRouter.tsx
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Home from "pages/Home";
 import Spacs from "pages/Spacs";
 import PageNotFound from "pages/PageNotFound";
+import TrackedPage from "utils/GoogleAnalytics";
 
 // This componenet looks through all its children elements (Routes) and tries to render the
 // first route where the path of that route matches the current URL. In this case, we want
@@ -10,10 +11,10 @@ import PageNotFound from "pages/PageNotFound";
 export default function PageRouter() {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/spacs" component={Spacs} />
+      <Route exact path="/" component={TrackedPage(Home)} />
+      <Route exact path="/spacs" component={TrackedPage(Spacs)} />
       <Route exact path="/discord" render={_redirectToDiscord} />
-      <Route component={PageNotFound} />
+      <Route component={TrackedPage(PageNotFound)} />
     </Switch>
   );
 }
