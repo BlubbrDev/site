d77fba87328c052b9cc4d6007708ff436c4e2159
Justin Shaw <realjustinshaw@gmail.com>
2021-03-21 17:47:27 -0700

    remove unused scripts

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "App";
-import reportWebVitals from "utils/reportWebVitals";
-import * as serviceWorker from "utils/serviceWorker";
+// import reportWebVitals from "utils/reportWebVitals";
+// import * as serviceWorker from "utils/serviceWorker";
 
 import "assets/scss/style.scss";
 
@@ -20,9 +20,9 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
