d58049ddd7be8ed77d1ed09d1bb3921b2461af2f
Justin Shaw <realjustinshaw@gmail.com>
2021-03-22 11:54:31 -0700

    added comments to utils folder

--- src/utils/ScrollReveal.tsx
+++ src/utils/ScrollReveal.tsx
@@ -8,92 +8,129 @@ import React, {
 } from "react";
 import { throttle } from "lodash";
 
-type _ScrollerProps = {
+/**
+ * Defines the shape of the props passed into the scroller.
+ */
+interface _ScrollerProps {
   children: ReactNode | ReactNodeArray;
-};
+}
 
+/**
+ * The scroller that controlls when elements are revealed based on the current scroll position.
+ */
 const _Scroller = React.forwardRef((props: _ScrollerProps, ref) => {
+  const throttleWait = 30; // The number of milliseconds to throttle invocations to.
   const [viewportHeight, setViewportheight] = useState(window.innerHeight);
   const [revealEl, setRevealel] = useState<NodeListOf<Element>>();
+  
+  /**
+   * Reveals the elements when the window loads.
+   */
+  useEffect(() => {
+    handleListeners();
+    revealElements();
+  }, [viewportHeight]);
+  
+    /**
+     * Exposes an initializer callback function that sets the reveal for the scroll reveal.
+     */
+    useImperativeHandle(ref, () => ({
+      init() {
+        const query = "[class*=reveal-]";
+        setRevealel(document.querySelectorAll(query) as NodeListOf<Element>);
+      },
+    }));
+  
+    /**
+     * When the reveal element loads, checks to see if the reveal has been completed and if not, it
+     * will set an event listener on the scroll and resize emitters.
+     */
+    useEffect(() => {
+      if (typeof revealEl !== "undefined" && revealEl.length > 0) {
+        if (!checkComplete()) {
+          window.addEventListener("scroll", handleScroll);
+          window.addEventListener("resize", handleResize);
+        }
+        revealElements();
+      }
+    }, [revealEl]);
 
+  /**
+   * Checks if the reveal has completed (all the elements that should be visible are visible).
+   * @returns whether or not the revealed elements are all visible
+   */
   const checkComplete = () => {
     const query = "[class*=reveal-].is-revealed";
     if (!revealEl) return false;
     return revealEl.length <= document.querySelectorAll(query).length;
   };
 
-  const elementIsVisible = (el: Element, offset: number) => {
+  /**
+   * Returns whether or not the given element is within the given offset of being visible.
+   * @param el the element that we want to check the visibility of
+   * @param offset the offset of the viewport that should be deemed "visible"
+   * @returns whether or not the given element is visible
+   */
+  const isVisible = (el: Element, offset: number) => {
     return el.getBoundingClientRect().top <= viewportHeight - offset;
   };
 
+  /**
+   * Reveals the elements that should be revealed based on the current scroll position.
+   */
   const revealElements = () => {
     if (checkComplete()) return;
     if (!revealEl) return;
+    const delay = "data-reveal-delay";
+    const offset = "data-reveal-offset";
+    const container = "data-reveal-container";
+    const revealed = "is-revealed";
     for (let i = 0; i < revealEl.length; i++) {
       const el: Element = revealEl[i];
-      const revealDelay = parseInt(
-        el.getAttribute("data-reveal-delay") as string
-      );
-      const revealOffset: number = el.getAttribute("data-reveal-offset")
-        ? parseInt(el.getAttribute("data-reveal-offset") as string)
+      const revealDelay = parseInt(el.getAttribute(delay) as string);
+      const revealOffset: number = el.getAttribute(offset)
+        ? parseInt(el.getAttribute(offset) as string)
         : 200;
-      const listenedEl = el.getAttribute("data-reveal-container")
-        ? (el.closest(
-            el.getAttribute("data-reveal-container") as string
-          ) as Element)
+      const listenedEl = el.getAttribute(container)
+        ? (el.closest(el.getAttribute(container) as string) as Element)
         : el;
       if (
-        elementIsVisible(listenedEl, revealOffset) &&
-        !el.classList.contains("is-revealed")
+        isVisible(listenedEl, revealOffset) &&
+        !el.classList.contains(revealed)
       ) {
         if (revealDelay && revealDelay !== 0) {
-          setTimeout(function () {
-            el.classList.add("is-revealed");
-          }, revealDelay);
+          setTimeout(() => el.classList.add(revealed), revealDelay);
         } else {
-          el.classList.add("is-revealed");
+          el.classList.add(revealed);
         }
       }
     }
   };
 
-  useImperativeHandle(ref, () => ({
-    init() {
-      setRevealel(
-        document.querySelectorAll("[class*=reveal-]") as NodeListOf<Element>
-      );
-    },
-  }));
-
-  useEffect(() => {
-    if (typeof revealEl !== "undefined" && revealEl.length > 0) {
-      if (!checkComplete()) {
-        window.addEventListener("scroll", handleScroll);
-        window.addEventListener("resize", handleResize);
-      }
-      revealElements();
-    }
-  }, [revealEl]);
-
+  /**
+   * Removes the event listeners for onScroll and onResize event emitters.
+   */
   const handleListeners = () => {
     if (!checkComplete()) return;
     window.removeEventListener("scroll", handleScroll);
     window.removeEventListener("resize", handleResize);
   };
 
+  /**
+   * Handles a scroll event (throttled to only execute once every throttleWait milliseconds).
+   */
   const handleScroll = throttle(() => {
     handleListeners();
     revealElements();
-  }, 30);
+  }, throttleWait);
 
+  /**
+   * Handles a resize event (throttled to only execute once every throttleWait milliseconds).
+   */
   const handleResize = throttle(() => {
     setViewportheight(window.innerHeight);
-  }, 30);
+  }, throttleWait);
 
-  useEffect(() => {
-    handleListeners();
-    revealElements();
-  }, [viewportHeight]);
 
   return <>{props.children}</>;
 });
