9ae29954596927599e5301ee72f14dbee8c15a35
Justin Shaw <realjustinshaw@gmail.com>
2021-03-21 18:09:54 -0700

    updated GA tracked page component

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import LayoutDefault from "layouts/LayoutDefault";
 import Animation from "components/elements/Animation";
+import TrackedPage from "utils/GoogleAnalytics";
 
 export default function PageNotFound(): JSX.Element {
   return (
-    <LayoutDefault>
-      <Animation
-        src="./page_not_found.riv"
-        style={{ height: "calc(100vh - 286px)", marginTop: "80px" }}
-      ></Animation>
-    </LayoutDefault>
+    <TrackedPage>
+      <LayoutDefault>
+        <Animation
+          src="./page_not_found.riv"
+          style={{ height: "calc(100vh - 286px)", marginTop: "80px" }}
+        ></Animation>
+      </LayoutDefault>
+    </TrackedPage>
   );
 }
--- src/utils/PageRouter.tsx
+++ src/utils/PageRouter.tsx
@@ -3,7 +3,6 @@ import { Switch, Route } from "react-router-dom";
 import Home from "pages/Home";
 import Spacs from "pages/Spacs";
 import PageNotFound from "pages/PageNotFound";
-import TrackedPage from "utils/GoogleAnalytics";
 
 // This componenet looks through all its children elements (Routes) and tries to render the
 // first route where the path of that route matches the current URL. In this case, we want
@@ -11,10 +10,10 @@ import TrackedPage from "utils/GoogleAnalytics";
 export default function PageRouter(): JSX.Element {
   return (
     <Switch>
-      <Route exact path="/" component={TrackedPage(Home)} />
-      <Route exact path="/spacs" component={TrackedPage(Spacs)} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/spacs" component={Spacs} />
       <Route exact path="/discord" render={_redirectToDiscord} />
-      <Route component={TrackedPage(PageNotFound)} />
+      <Route component={PageNotFound} />
     </Switch>
   );
 }
