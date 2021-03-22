e1f33d8267ce0db5a1abd4b4781156fc999c85a8
Justin Shaw <realjustinshaw@gmail.com>
2021-03-22 11:24:12 -0700

    removed web vitals reporting

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "App";
-// import reportWebVitals from "utils/reportWebVitals";
 // import * as serviceWorker from "utils/serviceWorker";
 
 import "assets/scss/style.scss";
@@ -16,11 +15,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
