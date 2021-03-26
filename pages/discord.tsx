6b1e6709e77f6ee1315bc2b91cc4b0734bacfa57
Justin Shaw <realjustinshaw@gmail.com>
2021-03-26 16:39:07 -0700

    updated discord redirect comments

--- pages/discord.tsx
+++ pages/discord.tsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 
+/**
+ * Redirects the user to the discord invite URL stored as an environment variable.
+ */
 export default function Discord() {
   useEffect(() => {
     window.location.assign(process.env.NEXT_PUBLIC_DISCORD_URL);
