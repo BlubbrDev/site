afb22527352d0ec75e28914b353037b01102036a
Justin Shaw <realjustinshaw@gmail.com>
2021-04-12 21:58:57 -0700

    ugly but it works

--- src/components/cta/newsletter.tsx
+++ src/components/cta/newsletter.tsx
@@ -1,35 +1,9 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 interface NewsletterProps {
   title: string;
   subtitle: string;
 }
 
 export default function Newsletter({ title, subtitle }: NewsletterProps) {
-  const [isSubmitted, setIsSumbitted] = useState(false);
-  useEffect(() => {
-    console.log("starting");
-    let url =
-      "https://blubbr.us1.list-manage.com/subscribe/post?u=620a18422b78481eb1e7e0320&amp;id=bcb1f8eb89";
-    let form = document.getElementById("mc-form") as HTMLFormElement;
-    form.addEventListener("submit", (event) => {
-      console.log("submitting");
-      let formData = new FormData(form);
-      axios({
-        method: "POST",
-        url: url,
-        data: formData,
-        headers: {
-          "Content-Type": "multipart/form-data",
-          crossdomain: true,
-          "Access-Control-Allow-Origin": "*",
-        },
-      })
-        .then((res) => res.status == 200 && setIsSumbitted(true))
-        .catch((err) => console.error(err));
-      event.preventDefault();
-    });
-  }, []);
   return (
     <section id="newsletter" className="mt-10 py-32 bg-blue-darkest bg-repeat">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
@@ -38,31 +12,23 @@ export default function Newsletter({ title, subtitle }: NewsletterProps) {
           <span className="block text-blue-light font-medium">{subtitle}</span>
         </h2>
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-          <form className="mr-4" id="mc-form" name="mc-embedded-subscribe-form">
+          <form className="mr-4" id="mc-form" name="mc-embedded-subscribe-form" action="https://blubbr.us1.list-manage.com/subscribe/post?u=620a18422b78481eb1e7e0320&amp;id=bcb1f8eb89" method="POST" target="_blank">
             <div className="flex flex-row" id="mc_embed_signup_scroll">
-              {!isSubmitted && (
-                <input
-                  className="inline-flex px-4 mx-4 py-2 w-full font-light shadow-md sm:text-md rounded-lg outline-none"
-                  type="text"
-                  name="EMAIL"
-                  id="mce-EMAIL"
-                  placeholder="Enter your email"
-                  required
-                />
-              )}
-              {isSubmitted ? (
-                <div className="inline-flex items-center justify-center px-5 py-2 outline-none text-md font-light rounded-md text-white bg-blue-darker">
-                  Subscribed!
-                </div>
-              ) : (
-                <input
-                  className="inline-flex items-center justify-center px-5 py-2 outline-none text-md font-light rounded-md text-white bg-blue-dark hover:bg-blue-darker"
-                  type="submit"
-                  value="Subscribe"
-                  name="subscribe"
-                  id="mc-embedded-subscribe"
-                />
-              )}
+              <input
+                className="inline-flex px-4 mx-4 py-2 w-full font-light shadow-md sm:text-md rounded-lg outline-none"
+                type="text"
+                name="EMAIL"
+                id="mce-EMAIL"
+                placeholder="Enter your email"
+                required
+              />
+              <input
+                className="inline-flex items-center justify-center px-5 py-2 outline-none text-md font-light rounded-md text-white bg-blue-dark hover:bg-blue-darker"
+                type="submit"
+                value="Subscribe"
+                name="subscribe"
+                id="mc-embedded-subscribe"
+              />
             </div>
           </form>
         </div>
--- src/pages/api/newsletter.js
+++ src/pages/api/newsletter.js
@@ -1,5 +1,15 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default (req, res) => {
-  res.status(200).json({ Hey: "Welcome to Blubbr" });
+export default async (req, res) => {
+  axios({
+    method: "POST",
+    url:
+      "https://blubbr.us1.list-manage.com/subscribe/post?u=620a18422b78481eb1e7e0320&amp;id=bcb1f8eb89",
+    data: formData,
+    headers: {
+      crossdomain: true,
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "multipart/form-data",
+    },
+  })
+    .then((res) => (res.status = 200))
+    .catch((err) => console.error(err));
 };
